e86bbc054d7709de1dea5f015e1a5b4c57ad58b7
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-03-08 11:21:17 +0100

    Create the first instance of the Sale class

--- src/main/java/edu/ntnu/idatt1002/Sale.java
+++ src/main/java/edu/ntnu/idatt1002/Sale.java
@@ -1,4 +1,88 @@
 package edu.ntnu.idatt1002;
 
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * The class represents a sale for the user.
+ */
 public class Sale {
+  private final Contact customer;
+  private final Date date;
+  private final String product;
+  private final String receiverAccount;
+  private final double amount;
+
+  /**
+   * Allocates a Sale object and initialises it.
+   *
+   * @param customer The contact that bought a service from the user.
+   * @param date The date of purchase.
+   * @param product The product that has been sold to the customer.
+   * @param receiverAccount The account that should receive the money from the sale.
+   * @param amount The amount of money the customer is set to pay from the sale.
+   */
+  public Sale(Contact customer, Date date, String product, String receiverAccount, double amount)
+          throws NullPointerException, IllegalArgumentException {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("A sale's amount cannot be 0 or less");
+    }
+    if (receiverAccount.length() != 11) {
+      throw new IllegalArgumentException("The account number has to be 11 numbers long");
+    }
+
+
+    this.customer = Objects.requireNonNull(customer, "Customer cannot be null");
+    this.date = Objects.requireNonNull(date, "Date cannot be null");
+    this.product = Objects.requireNonNull(product, "Product cannot be null");
+    this.receiverAccount = Objects.requireNonNull(receiverAccount, "Account number cannot be null");
+    this.amount = amount;
+  }
+
+  /**
+   * Gets the customer of the sale.
+   *
+   * @return The customer of the sale.
+   */
+  public Contact getCustomer() {
+    return customer;
+  }
+
+  /**
+   * Gets the sales date.
+   *
+   * @return The sales date.
+   */
+  public Date getDate() {
+    return date;
+  }
+
+  /**
+   * Gets the description of the product that has been sold.
+   *
+   * @return The name of the product that has been sold.
+   */
+  public String getProduct() {
+    return product;
+  }
+
+  /**
+   * Gets the account number of the account that will receive the money.
+   *
+   * @return The account number that receives the money.
+   */
+  public String getReceiverAccount() {
+    return receiverAccount;
+  }
+
+  /**
+   * Gets the amount of money the customer has to pay for the product.
+   *
+   * @return The amount of money the customer has to pay for the product.
+   */
+  public double getAmount() {
+    return amount;
+  }
+
+
 }
