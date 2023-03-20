07b3d113c61863512a7776125f734a82ba62b355
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-03-20 10:17:35 +0100

    Change file names for storing budgets and contacts
    Fix an error where the BudgetRegister and ContactRegister classes would save their info in the expenses.txt file

--- src/main/java/edu/ntnu/idatt1002/registers/BudgetRegister.java
+++ src/main/java/edu/ntnu/idatt1002/registers/BudgetRegister.java
@@ -3,7 +3,7 @@ package edu.ntnu.idatt1002.registers;
 import edu.ntnu.idatt1002.Expense;
 
 public class BudgetRegister extends Register<Expense> {
-  private static final String FILE_NAME = "expenses.txt";
+  private static final String FILE_NAME = "budgets.txt";
 
   public BudgetRegister() {
     super(FILE_NAME);
--- src/main/java/edu/ntnu/idatt1002/registers/ContactRegister.java
+++ src/main/java/edu/ntnu/idatt1002/registers/ContactRegister.java
@@ -3,7 +3,7 @@ package edu.ntnu.idatt1002.registers;
 import edu.ntnu.idatt1002.Expense;
 
 public class ContactRegister extends Register<Expense> {
-  private static final String FILE_NAME = "expenses.txt";
+  private static final String FILE_NAME = "contacts.txt";
 
   public ContactRegister() {
     super(FILE_NAME);
