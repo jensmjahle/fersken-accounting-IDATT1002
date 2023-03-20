1ef1ef5758467edccb2e3375269657c520d5c432
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-03-20 14:09:17 +0100

    Add the file name of the register as a constructor parameter
    - Remove the previous constant containing the file location to have more reusable code
    - Enables testing of the different register classes

--- src/main/java/edu/ntnu/idatt1002/registers/ExpenseRegister.java
+++ src/main/java/edu/ntnu/idatt1002/registers/ExpenseRegister.java
@@ -3,10 +3,9 @@ package edu.ntnu.idatt1002.registers;
 import edu.ntnu.idatt1002.Expense;
 
 public class ExpenseRegister extends Register<Expense> {
-  private static final String FILE_NAME = "expenses.txt";
 
-  public ExpenseRegister() {
-    super(FILE_NAME);
+  public ExpenseRegister(String fileName) {
+    super(fileName);
   }
 
 }
