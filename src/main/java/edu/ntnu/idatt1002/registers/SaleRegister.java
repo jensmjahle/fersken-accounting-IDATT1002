5a0be540af11706d81360e3f3068c714d01878e8
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-03-22 13:07:09 +0100

    Add missing javadoc to all Register classes

--- src/main/java/edu/ntnu/idatt1002/registers/SaleRegister.java
+++ src/main/java/edu/ntnu/idatt1002/registers/SaleRegister.java
@@ -2,8 +2,17 @@ package edu.ntnu.idatt1002.registers;
 
 import edu.ntnu.idatt1002.Sale;
 
+/**
+ * This class represents a sale register that stores object of type "Sale".
+ * The class extends from class Register.
+ */
 public class SaleRegister extends Register<Sale> {
 
+  /**
+   * Constructor that creates an instance of the class.
+   *
+   * @param fileName file name of the file to store data.
+   */
   public SaleRegister(String fileName) {
     super(fileName);
   }
