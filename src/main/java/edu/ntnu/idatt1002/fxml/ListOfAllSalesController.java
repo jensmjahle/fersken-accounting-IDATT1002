c7a25d9e2735452e91e22d0c68b27884f2602189
Henrik Tefre <henritef@stud.ntnu.no>
2023-03-23 20:32:59 +0100

    add controller classes for RegisterSale and RegisterExpense, ListOfAllExpenses and ListOfAllSales

--- src/main/java/edu/ntnu/idatt1002/fxml/ListOfAllSalesController.java
+++ src/main/java/edu/ntnu/idatt1002/fxml/ListOfAllSalesController.java
@@ -1,27 +1,69 @@
 package edu.ntnu.idatt1002.fxml;
 
 import edu.ntnu.idatt1002.PathUtility;
+import edu.ntnu.idatt1002.RegisterManager;
+import edu.ntnu.idatt1002.Sale;
+import edu.ntnu.idatt1002.registers.SaleRegister;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ListOfAllSalesController {
+public class ListOfAllSalesController implements Initializable {
 
-  private Stage stage;
-  private Scene scene;
-  private Parent root;
+  @FXML
+  private TableView salesTableView;
+  @FXML
+  private TableColumn customerTableColumn;
+  @FXML
+  private TableColumn dateTableColumn;
+  @FXML
+  private TableColumn accountTableColumn;
+  @FXML
+  private TableColumn amountTableColumn;
+  @FXML
+  private TableColumn productTableColumn;
 
   public void switchToMainMenuScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("MainMenu"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-    scene = new Scene(root);
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    Scene scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    SaleRegister saleRegister = RegisterManager.getInstance().getSaleRegister();
+
+    customerTableColumn.setCellValueFactory(
+            new PropertyValueFactory<>("customer"));
+
+    dateTableColumn.setCellValueFactory
+            (new PropertyValueFactory<>("date"));
+
+    accountTableColumn.setCellValueFactory(
+            new PropertyValueFactory<>("receiverAccount"));
+
+    amountTableColumn.setCellValueFactory(
+            new PropertyValueFactory<>("amount"));
+
+    productTableColumn.setCellValueFactory(
+            new PropertyValueFactory<>("product"));
+
+    for (Sale sale : saleRegister.getObjects()) {
+      salesTableView.getItems().add(sale);
+    }
   }
 }
--- src/main/java/edu/ntnu/idatt1002/fxml/RegisterExpenseController.java
+++ src/main/java/edu/ntnu/idatt1002/fxml/RegisterExpenseController.java
@@ -1,27 +1,58 @@
 package edu.ntnu.idatt1002.fxml;
 
+import edu.ntnu.idatt1002.Expense;
+import edu.ntnu.idatt1002.RegisterManager;
+import javafx.fxml.FXML;
 import edu.ntnu.idatt1002.PathUtility;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.util.Date;
 
 public class RegisterExpenseController {
 
-  private Stage stage;
-  private Scene scene;
-  private Parent root;
+  @FXML
+  private TextField amountTextField;
+  @FXML
+  private DatePicker datePicker;
+  @FXML
+  private TextField productTextField;
 
   public void switchToMainMenuScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("MainMenu"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-    scene = new Scene(root);
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    Scene scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
+  }
+  @FXML
+  public void createExpense() {
+    // String supplier = supplierTextField.getText();
+    Date date = java.sql.Date.valueOf(datePicker.getValue());
+    double amount = (Double.parseDouble(amountTextField.getText()));
+    String product = productTextField.getText();
+
+    // Expense newExpense = new Expense(supplier, amount, date, product);
+    try {
+      Expense newExpense = new Expense(amount, date, product);
+      RegisterManager.getInstance().getExpenseRegister().addObject(newExpense);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    clearAllFields();
+  }
+  @FXML
+  public void clearAllFields() {
+    datePicker.setValue(null);
+    amountTextField.clear();
+    productTextField.clear();
   }
 }
