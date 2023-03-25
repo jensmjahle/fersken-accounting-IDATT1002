7af49b8c2408b09caa20bbac8d8bee697549abd8
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-03-25 11:53:01 +0100

    Implement Statistics class to front page chart
    -Change bar chart to a line chart
    -Use statistic data in chart to make it show actual values

--- src/main/java/edu/ntnu/idatt1002/fxml/MainMenuController.java
+++ src/main/java/edu/ntnu/idatt1002/fxml/MainMenuController.java
@@ -1,55 +1,76 @@
 package edu.ntnu.idatt1002.fxml;
 
 import edu.ntnu.idatt1002.PathUtility;
-import java.io.File;
+import edu.ntnu.idatt1002.RegisterManager;
+import edu.ntnu.idatt1002.Statistics;
+import java.io.IOException;
+import java.net.URL;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class MainMenuController implements Initializable {
 
   private Stage stage;
   private Scene scene;
-  private Parent root;
+
   @FXML
-  private BarChart barChart;
+  private LineChart<String, Double> lineChart;
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
-    XYChart.Series series1 = new XYChart.Series();
 
-    series1.setName("Inntekt");
-    series1.getData().add(new XYChart.Data("Januar", 0));
-    series1.getData().add(new XYChart.Data("Februar", 0));
-    series1.getData().add(new XYChart.Data("Mars", 0));
-    series1.getData().add(new XYChart.Data("April", 0));
-    series1.getData().add(new XYChart.Data("Mai", 0));
-    series1.getData().add(new XYChart.Data("Juni", 0));
-    series1.getData().add(new XYChart.Data("Juli", 0));
-    series1.getData().add(new XYChart.Data("August", 0));
-    series1.getData().add(new XYChart.Data("September", 0));
-    series1.getData().add(new XYChart.Data("Oktober", 0));
-    series1.getData().add(new XYChart.Data("November", 0));
-    series1.getData().add(new XYChart.Data("Desember", 0));
+    final CategoryAxis xAxis = new CategoryAxis();
+    final NumberAxis yAxis = new NumberAxis();
+    xAxis.setLabel("Month");
+    yAxis.setLabel("Kr");
 
-    barChart.getData().add(series1);
+    final XYChart.Series<String, Double> income = new XYChart.Series<>();
+    final XYChart.Series<String, Double> expenses = new XYChart.Series<>();
+    final XYChart.Series<String, Double> difference = new XYChart.Series<>();
+
+    Statistics statistics = new Statistics(RegisterManager.getInstance());
+    lineChart.setTitle("Yearly result");
+
+    income.setName("Income");
+    expenses.setName("Expenses");
+    difference.setName("Difference");
+
+    for (int i = 0; i < 12; i++) {
+      income.getData()
+          .add(new XYChart.Data<>(Month.of(i + 1).toString(), statistics.getSaleTotalForMonth(i)));
+      expenses.getData().add(
+          new XYChart.Data<>(Month.of(i + 1).toString(), -statistics.getExpenseTotalForMonth(i)));
+      difference.getData()
+          .add(new XYChart.Data<>(Month.of(i + 1).toString(), statistics.getDifferenceForMonth(i)));
+    }
+
+    lineChart.getData().addAll(Arrays.asList(expenses, difference, income));
+    lineChart.setAnimated(false);
+    lineChart.setCreateSymbols(false);
+    lineChart.setPadding(new Insets(0, 0, 0, 0));
+    lineChart.getXAxis().setAutoRanging(true);
+    lineChart.getYAxis().setAutoRanging(true);
+
   }
 
   public void switchToCreateBudgetScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("CreateBudget"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
@@ -57,7 +78,7 @@ public class MainMenuController implements Initializable {
 
   public void switchToCreateCustomerScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("CreateCustomer"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
@@ -65,7 +86,7 @@ public class MainMenuController implements Initializable {
 
   public void switchToCreateSupplierScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("CreateSupplier"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
@@ -73,7 +94,7 @@ public class MainMenuController implements Initializable {
 
   public void switchToListOfAllBudgetsScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("ListOfAllBudgets"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
@@ -81,7 +102,7 @@ public class MainMenuController implements Initializable {
 
   public void switchToListOfAllCustomersScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("ListOfAllCustomers"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
@@ -89,7 +110,7 @@ public class MainMenuController implements Initializable {
 
   public void switchToListOfAllExpensesScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("ListOfAllExpenses"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
@@ -97,7 +118,7 @@ public class MainMenuController implements Initializable {
 
   public void switchToListOfAllSalesScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("ListOfAllSales"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
@@ -105,7 +126,7 @@ public class MainMenuController implements Initializable {
 
   public void switchToListOfAllSuppliersScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("ListOfAllSuppliers"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
@@ -113,7 +134,7 @@ public class MainMenuController implements Initializable {
 
   public void switchToRegisterExpenseScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("RegisterExpense"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
@@ -121,7 +142,7 @@ public class MainMenuController implements Initializable {
 
   public void switchToRegisterSaleScene(MouseEvent event) throws IOException {
     Parent root = FXMLLoader.load(PathUtility.getResourcePath("RegisterSale"));
-    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
