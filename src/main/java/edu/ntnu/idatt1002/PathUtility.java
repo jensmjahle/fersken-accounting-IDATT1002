b8fc835d40a31b9599032a43b2d457c18940e859
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-04-20 11:18:17 +0200

    Add missing JavaDoc to PathUtility

--- src/main/java/edu/ntnu/idatt1002/PathUtility.java
+++ src/main/java/edu/ntnu/idatt1002/PathUtility.java
@@ -4,11 +4,26 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * Public class for getting resolving resource paths for fxml files.
+ */
 public class PathUtility {
 
-  private PathUtility(){
+  /**
+   * Private constructor that prohibits the creation of a PathUtility object.
+   */
+  private PathUtility() {
     throw new UnsupportedOperationException("Cannot create path utility object");
   }
+
+  /**
+   * Gets the resource path of the specified location.
+   *
+   * @param location Name of the fxml file located in the pages folder in resources.
+   * @return The resource path of the specified file.
+   * @throws MalformedURLException If the resource path is invalid
+   * @throws NullPointerException  If a file cannot be created from the path because of an error.
+   */
   public static URL getResourcePath(String location) throws MalformedURLException {
     String partOfPath = "src/main/resources/pages/" + location + ".fxml";
     return new File(partOfPath).toURI().toURL();
