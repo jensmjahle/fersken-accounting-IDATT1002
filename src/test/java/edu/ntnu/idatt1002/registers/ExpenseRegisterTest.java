7ded593642e007a6a3e52579e20f6844e9ee1b20
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-04-20 11:15:32 +0200

    Refactor tests with new constructors to account for changes

--- src/test/java/edu/ntnu/idatt1002/registers/ExpenseRegisterTest.java
+++ src/test/java/edu/ntnu/idatt1002/registers/ExpenseRegisterTest.java
@@ -32,7 +32,7 @@ class ExpenseRegisterTest {
 
     expenseRegister = new ExpenseRegister(file.getPath());
     date = new Date();
-    contact = new Contact("name", "email", "street", 12, "12345678", "11111111111", "7043");
+    contact = new Contact("name", "email@email.com", "street", 12, "12345678", "11111111111", "7043", "1234");
     expense = new Expense(contact, 150, date, "product");
   }
 
