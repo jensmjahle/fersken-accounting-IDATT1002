abebab696688edf735137d596b5c577fb92a030a
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-04-24 11:11:02 +0200

    Refactor expense to adhere to Checkstyle guidelines

--- src/main/java/edu/ntnu/idatt1002/storageitems/Expense.java
+++ src/main/java/edu/ntnu/idatt1002/storageitems/Expense.java
@@ -1,6 +1,5 @@
 package edu.ntnu.idatt1002.storageitems;
 
-import edu.ntnu.idatt1002.storageitems.Contact;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
@@ -68,12 +67,10 @@ public class Expense implements Serializable {
   }
 
   /**
-   * Constructor for an expense object with o
+   * Constructor for an expense object with only amount and product.
    *
-   * @param nameOfContact information about the contact. As a Contact
-   * @param amount        of money in NOK. As a double
-   * @param date          of the expense. As a Date
-   * @param product       the product connected to the expense.
+   * @param amount  of money in NOK.
+   * @param product the product connected to the expense.
    */
   public Expense(double amount, String product)
       throws IllegalArgumentException, NullPointerException {
